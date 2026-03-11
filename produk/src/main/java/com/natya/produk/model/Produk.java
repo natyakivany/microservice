49334bfe6d6064c962401ca0c683ba37c3952b02
natyakivany <natiakifani@gmail.com>
2026-03-11 09:15:28 +0700

    Add files via upload

---
+++ produk/src/main/java/com/natya/produk/model/Produk.java
@@ -0,0 +1,19 @@
+package com.natya.produk.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Produk {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nama;
+    private String satuan;
+    private String harga;
+}
---
+++ produk/src/main/java/com/natya/produk/service/OrderService.java
@@ -0,0 +1,36 @@
+package com.natya.produk.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natya.produk.repository.OrderRepository;
+import com.natya.produk.model.Order;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Order createOrder(Order order) {
+    order.hitungTotal();
+    return orderRepository.save(order);
+}
+
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    public void deleteOrder(Long id) {
+        orderRepository.deleteById(id);
+    }
+
+    public void deleteByPelangganId(Integer pelangganId) {
+        orderRepository.deleteByPelangganId(pelangganId);
+    }
+}
